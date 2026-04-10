5782935fc2e93d0fda14cccfa7e0a133b9993184
Van Phuong <email_cua_ban@gmail.com>
2026-04-10 19:07:48 +0700

    Step 1: Khoi tao project

---
+++ BT DB First/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using BT_DB_First.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BT_DB_First.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BT DB First/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BT_DB_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
